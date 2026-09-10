2fb3b7827dfbcd37ba7c158eb1777ef81669c030
djurslinn <djurslinnjameskm@gmail.com>
2026-09-10 19:42:49 +0530

    fixed 404 error

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   resolve: {
     tsconfigPaths: true,
   },
   plugins: [
-    tanstackStart({
-      server: { preset: "vercel" },
-    }),
+    tanstackStart(),
+    nitro({ preset: 'vercel' }),
     react(),
     tailwindcss(),
   ],
